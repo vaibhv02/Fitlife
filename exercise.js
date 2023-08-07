32bf978357b6f7eb8706bfc6371471dcfb939e00
Shivam Awasthi <shivam.awasthi@hypd.in>
2023-08-08 04:23:33 +0530

    "added style and cards and pagination"

--- exercise.js
+++ exercise.js
@@ -1,12 +1,12 @@
 function saveSelection(variable, value) {
 	window[variable] = value;
-  }
-  fetchExercises();
-  async function fetchExercises() {
+}
+fetchExercises();
+async function fetchExercises() {
 	const url = 'https://musclewiki.p.rapidapi.com/exercises/attributes';
 	const response = await fetch(url, {
-	  method: 'GET',
-	  headers: getHeaders()  
+		method: 'GET',
+		headers: getHeaders()
 	});
 	const data = await response.json();
 
@@ -15,41 +15,82 @@ function saveSelection(variable, value) {
 	populateDropdown('forceDropdown', data.forces);
 	populateDropdown('muscleDropdown', data.muscles);
 
-  }
-  function getHeaders() {
+}
+function getHeaders() {
 	return {
-	  'X-RapidAPI-Key': '885fb42166msh42252d84b68715fp11ffbajsnb0f9b1444506',
-	  'X-RapidAPI-Host': 'musclewiki.p.rapidapi.com'
+		'X-RapidAPI-Key': '885fb42166msh42252d84b68715fp11ffbajsnb0f9b1444506',
+		'X-RapidAPI-Host': 'musclewiki.p.rapidapi.com'
 	};
-  }
+}
 
-  function populateDropdown(id, options) {
+function populateDropdown(id, options) {
 	const dropdown = document.getElementById(id);
 	options.forEach(option => {
-	  const opt = document.createElement('option');
-	  opt.value = option;
-	  opt.textContent = option;
-	  dropdown.appendChild(opt);
+		const opt = document.createElement('option');
+		opt.value = option;
+		opt.textContent = option;
+		dropdown.appendChild(opt);
 	});
-  }
-  document.getElementById('searchBtn').addEventListener('click', async () => {
+}
+document.getElementById('searchBtn').addEventListener('click', async () => {
 	let url = 'https://musclewiki.p.rapidapi.com/exercises?';
-	if(window['selectedCategory']) {
-	  url += `category=${window['selectedCategory']}&`;
+	if (window['selectedCategory']) {
+		url += `category=${window['selectedCategory']}&`;
 	}
-	if(window['selectedDifficulty']) {
-	  url += `difficulty=${window['selectedDifficulty']}&`;
+	if (window['selectedDifficulty']) {
+		url += `difficulty=${window['selectedDifficulty']}&`;
 	}
-	if(window['selectedForce']) {
-	  url += `force=${window['selectedForce']}&`;
+	if (window['selectedForce']) {
+		url += `force=${window['selectedForce']}&`;
 	}
-	if(window['selectedMuscle']) {
-	  url += `muscle=${window['selectedMuscle']}`;
+	if (window['selectedMuscle']) {
+		url += `muscle=${window['selectedMuscle']}`;
 	}
 	const response = await fetch(url, {
-	  method: 'GET',
-	  headers: getHeaders()
+		method: 'GET',
+		headers: getHeaders()
 	});
 	const data = await response.json();
-	document.getElementById('output').innerHTML = JSON.stringify(data);
-  });
+	const exercises = data
+	console.log(exercises)
+	const perPage = 5;
+	let currentPage = 1;
+	const totalPages = Math.ceil(exercises.length / perPage);
+
+	function displayCards() {
+		const start = (currentPage - 1) * perPage;
+		const end = start + perPage;
+		const cards = exercises.slice(start, end);
+		document.getElementById('cards').innerHTML = '';
+		cards.forEach(exercise => {
+		  const card = document.createElement('div');
+		  card.classList.add('card');
+		  card.innerHTML = `
+		  <h3 class="content tooltip" title="Details:${exercise.details}">${exercise.exercise_name}</h3>
+		  <li>${exercise.steps}</li>
+		  <a href="${exercise.videoURL}" target="_blank">Video Links</a>
+		`;
+	
+		  document.getElementById('cards').appendChild(card);
+		});
+	  }
+	function displayPagination() {
+		const pagination = document.getElementById('pagination');
+		pagination.innerHTML = '';
+		for (let i = 1; i <= totalPages; i++) {
+			const button = document.createElement('button');
+			button.textContent = i;
+			if (i === currentPage) {
+				button.classList.add('active');
+			}
+			button.addEventListener('click', () => {
+				currentPage = i;
+				displayCards();
+				displayPagination();
+			});
+			pagination.appendChild(button);
+		}
+	}
+	displayCards();
+	displayPagination();
+});

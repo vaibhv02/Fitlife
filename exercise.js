28fdb0faa612f06fc8810f6063b0770ffb5e9299
Shivam Awasthi <shivam.awasthi@hypd.in>
2023-08-08 02:38:12 +0530

    "dropdown created with response population"

--- exercise.js
+++ exercise.js
@@ -1,14 +1,55 @@
-const xhr = new XMLHttpRequest();
+function saveSelection(variable, value) {
+	window[variable] = value;
+  }
+  fetchExercises();
+  async function fetchExercises() {
+	const url = 'https://musclewiki.p.rapidapi.com/exercises/attributes';
+	const response = await fetch(url, {
+	  method: 'GET',
+	  headers: getHeaders()  
+	});
+	const data = await response.json();
 
-xhr.open('GET', 'https://musclewiki.p.rapidapi.com/exercises');
-xhr.setRequestHeader('X-RapidAPI-Key','69b5c25eecmsh72d9f07fc3dfc60p148098jsn8c6634af6304');
-xhr.setRequestHeader('X-RapidAPI-Host','musclewiki.p.rapidapi.com');
-xhr.onreadystatechange = () => {
-	if(xhr.readyState == 4 && xhr.status == 200) 
-	{
-		const response = JSON.parse(xhr.responseText)
-		console.log(response)
-		
+	populateDropdown('categoryDropdown', data.categories);
+	populateDropdown('difficultyDropdown', data.difficulties);
+	populateDropdown('forceDropdown', data.forces);
+	populateDropdown('muscleDropdown', data.muscles);
+
+  }
+  function getHeaders() {
+	return {
+	  'X-RapidAPI-Key': '885fb42166msh42252d84b68715fp11ffbajsnb0f9b1444506',
+	  'X-RapidAPI-Host': 'musclewiki.p.rapidapi.com'
+	};
+  }
+
+  function populateDropdown(id, options) {
+	const dropdown = document.getElementById(id);
+	options.forEach(option => {
+	  const opt = document.createElement('option');
+	  opt.value = option;
+	  opt.textContent = option;
+	  dropdown.appendChild(opt);
+	});
+  }
+  document.getElementById('searchBtn').addEventListener('click', async () => {
+	let url = 'https://musclewiki.p.rapidapi.com/exercises?';
+	if(window['selectedCategory']) {
+	  url += `category=${window['selectedCategory']}&`;
 	}
-}
-xhr.send();
+	if(window['selectedDifficulty']) {
+	  url += `difficulty=${window['selectedDifficulty']}&`;
+	}
+	if(window['selectedForce']) {
+	  url += `force=${window['selectedForce']}&`;
+	}
+	if(window['selectedMuscle']) {
+	  url += `muscle=${window['selectedMuscle']}`;
+	}
+	const response = await fetch(url, {
+	  method: 'GET',
+	  headers: getHeaders()
+	});
+	const data = await response.json();
+	document.getElementById('output').innerHTML = JSON.stringify(data);
+  });
